c77625b6f51e56e3930d18feb72c45080e3ae2f9
WEbcoder66 <lucatice@gmail.com>
2024-11-29 00:23:12 -0600

    admin login

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+// src/app/admin/page.tsx
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function AdminPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/admin/login');
+  }, [router]);
+
+  return null;
+}
